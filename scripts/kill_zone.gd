108f3ba85f2692d681ce382c29c82403d962643c
raihanmd <mraihanf471@gmail.com>
2024-05-02 06:32:16 +0700

    chore: cleaning the folder

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -3,11 +3,13 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
-	Engine.time_scale = .5
+	body.isAlive = false
+	body.velocity.y = -200
+	body.get_node("die_sfx").play()
 	body.get_node("CollisionShape2D").queue_free()
-	body.get_node("AnimatedSprite2D").play("hurt")
+	Engine.time_scale = .5
 	timer.start()
-
+	
 func _on_timer_timeout():
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+var isAlive: bool = true
+var can_dash: bool = true
+var direction: float
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var jump_sfx = $jump_sfx
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,11 +20,11 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump_sfx.play()
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
+	if isAlive:
+		direction = Input.get_axis("move_left", "move_right")
 	
 	if direction > 0:
 			animated_sprite.flip_h = false
@@ -42,3 +46,4 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
